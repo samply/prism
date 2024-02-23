5cd39330cbc2702798495d3bff677e920263afc7
Enola Knezevic <enola.knezevic@dkfz-heidelberg.com>
2024-02-23 17:48:34 +0100

    switched to tokio async and refactored

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,16 @@ use crate::errors::PrismError;
 use crate::{config::CONFIG, mr::MeasureReport};
 use std::collections::HashSet;
 use std::process::exit;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::time::SystemTime;
+use tokio::sync::Mutex;
 
 use axum::{
-    extract::State, 
+    extract::State,
     http::{header, StatusCode},
     response::{IntoResponse, Response},
     routing::post,
-    Json, 
-    Router,
+    Json, Router,
 };
 
 use base64::engine::general_purpose;
@@ -46,7 +46,7 @@ static BEAM_CLIENT: Lazy<BeamClient> = Lazy::new(|| {
     )
 });
 
-#[derive(Serialize, Deserialize, Clone, Debug)] 
+#[derive(Serialize, Deserialize, Clone, Debug)]
 struct LensQuery {
     id: MsgId,
     sites: Vec<String>,
@@ -123,7 +123,8 @@ pub async fn main() {
         .with_state(shared_state)
         .layer(cors);
 
-    let tcp_listener = tokio::net::TcpListener::bind(CONFIG.bind_addr).await
+    let tcp_listener = tokio::net::TcpListener::bind(CONFIG.bind_addr)
+        .await
         .expect("Unable to bind to listen port");
 
     axum::serve(tcp_listener, app.into_make_service())
@@ -137,49 +138,25 @@ async fn handle_get_criteria(
 ) -> Result<Response, (StatusCode, String)> {
     let mut criteria_groups: CriteriaGroups = CriteriaGroups::new();
 
-    match shared_state.criteria_cache.lock() {
-        Err(e) => {
-            error!("Mutex was poisoned: {:?}", e);
-        }
-        Ok(criteria_cache) => {
-            for site in query.clone().sites {
-                if criteria_cache.cache.contains_key(&site) {
-                    let maybe_criteria_group_time_from_cache = criteria_cache.cache.get(&site);
-                    match maybe_criteria_group_time_from_cache {
-                        //would just unwrap it, but unwrap can be a minefield after refactoring
-                        None => match shared_state.sites_to_query.lock() {
-                            Err(e) => {
-                                error!("Mutex was poisoned: {:?}", e);
-                            }
-                            Ok(mut sites_to_query) => {
-                                sites_to_query.insert(site);
-                            }
-                        },
-                        Some(criteria_groups_time_from_cache) => {
-                            if SystemTime::now()
-                                .duration_since(criteria_groups_time_from_cache.1)
-                                .unwrap()
-                                < CRITERIACACHE_TTL
-                            {
-                                criteria_groups = combine_groups_of_criteria_groups(
-                                    criteria_groups,
-                                    criteria_groups_time_from_cache.clone().0,
-                                );
-                            } else {
-                                match shared_state.sites_to_query.lock() {
-                                    Err(e) => {
-                                        error!("Mutex was poisoned: {:?}", e);
-                                    }
-                                    Ok(mut sites_to_query) => {
-                                        sites_to_query.insert(site);
-                                    }
-                                }
-                            }
-                        }
+    for site in query.clone().sites {
+        let criteria_groups_from_cache =
+            match shared_state.criteria_cache.lock().await.cache.get(&site) {
+                Some(cached) => {
+                    //we only use the cached results if they are not expired
+                    if SystemTime::now().duration_since(cached.1).unwrap() < CRITERIACACHE_TTL {
+                        Some(cached.0.clone())
+                    } else {
+                        None
                     }
                 }
+                None => None,
+            };
+
+            if let Some(cached_criteria_groups) = criteria_groups_from_cache {
+                criteria_groups = combine_groups_of_criteria_groups(criteria_groups, cached_criteria_groups);
+            } else {
+                shared_state.sites_to_query.lock().await.insert(site);
             }
-        }
     }
 
     let criteria_groups_json =
@@ -194,7 +171,7 @@ async fn handle_get_criteria(
 }
 
 async fn post_query(
-    mut tasks: std::sync::MutexGuard<'_, HashMap<MsgId, usize>>,
+    mut tasks: tokio::sync::MutexGuard<'_, HashMap<MsgId, usize>>,
     sites: &[impl ToString],
 ) -> Result<(), PrismError> {
     let task = create_beam_task(sites);
@@ -212,71 +189,38 @@ async fn query_sites(
     shared_state: &SharedState,
     sites: Option<&[impl ToString]>,
 ) -> Result<(), PrismError> {
-    if let Some(sites) = sites {
-        match shared_state.tasks.lock() {
-            Err(e) => {
-                error!("Mutex was poisoned: {:?}", e);
-                return Err(PrismError::PoisonedMutex(e.to_string()));
-            }
-            Ok(tasks) => {
-                post_query(tasks, sites).await?
-            }
-        }
-    }
 
-    match shared_state.sites_to_query.lock() {
-        Err(e) => {
-            error!("Mutex was poisoned: {:?}", e);
-            return Err(PrismError::PoisonedMutex(e.to_string()));
+match sites{
+        Some(sites) => {
+            post_query(shared_state.tasks.lock().await, sites).await?;
+        },
+        None => {
+            let mut locked_sites = shared_state.sites_to_query.lock().await;
+            let sites: Vec<String> = locked_sites.clone().into_iter().collect();
+            post_query(shared_state.tasks.lock().await, &sites).await?;
+            locked_sites.clear();
         }
-        Ok(mut sites_to_query) => {
-            match shared_state.tasks.lock() {
-                Err(e) => {
-                    error!("Mutex was poisoned: {:?}", e);
-                    return Err(PrismError::PoisonedMutex(e.to_string()));
-                }
-                Ok(tasks) => {
-                    let sites: Vec<_> = sites_to_query.clone().into_iter().collect();
-                    post_query(tasks, &sites).await?;
-                    sites_to_query.clear(); //only if no error
-                }
-            }
-        }
-    }
+    };
 
     Ok(())
 }
 
 async fn get_results(shared_state: SharedState) -> Result<(), PrismError> {
-    match shared_state.tasks.lock() {
-        Err(e) => {
-            error!("Mutex was poisoned: {:?}", e);
-            return Err(PrismError::PoisonedMutex(e.to_string()));
-        }
-        Ok(mut tasks) => {
-            match shared_state.criteria_cache.lock() {
-                Err(e) => {
-                    error!("Mutex was poisoned: {:?}", e);
-                    return Err(PrismError::PoisonedMutex(e.to_string()));
-                }
-                Ok(mut criteria_cache) => {
-                    for task in tasks.clone().into_iter() {
-                        let processed = process_results(task.0, &mut criteria_cache).await;
-                        match processed {
-                            Ok(()) => {
-                                tasks.remove(&task.0);
-                            }
-                            Err(e) => {
-                                error!("There has been an error getting results for task {}. Error: {}", task.0, e);
 
-                                if task.1 > 3 {
-                                    tasks.remove(&task.0); // 3 attempts enough, could even be 1
-                                } else {
-                                    tasks.entry(task.0).and_modify(|e| *e += 1);
-                                }
-                            }
-                        }
-                    }
+    let mut locked_tasks = shared_state.tasks.lock().await;
+    for task in locked_tasks.clone() {
+        let processed = process_results(task.0, &mut shared_state.criteria_cache.lock().await).await;
+        match processed {
+            Ok(()) => {
+                locked_tasks.remove(&task.0);
+            }
+            Err(e) => {
+                error!("There has been an error getting results for task {}. Error: {}", task.0, e);
+
+                if task.1 > 3 {
+                    locked_tasks.remove(&task.0); // 3 attempts enough, could even be 1
+                } else {
+                    locked_tasks.entry(task.0).and_modify(|e| *e += 1);
                 }
             }
         }
@@ -285,7 +229,10 @@ async fn get_results(shared_state: SharedState) -> Result<(), PrismError> {
     Ok(())
 }
 
-async fn process_results(task: MsgId, criteria_cache: &mut CriteriaCache) -> Result<(), PrismError> {
+async fn process_results(
+    task: MsgId,
+    criteria_cache: &mut tokio::sync::MutexGuard<'_, CriteriaCache>,
+) -> Result<(), PrismError> {
     let resp = BEAM_CLIENT
         .raw_beam_request(
             Method::GET,
